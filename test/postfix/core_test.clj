4f9dc7b1e1a58f138d0827384ccb566a69e2cc26
Geoff Shannon <geoffpshannon@gmail.com>
2014-09-15 04:46:57 -0700

    Add some real tests.
    Not all should pass now.

--- test/postfix/core_test.clj
+++ test/postfix/core_test.clj
@@ -1,7 +1,16 @@
 (ns postfix.core-test
   (:require [clojure.test :refer :all]
-            [postfix.core :refer :all]))
+            [postfix.core :refer [postfix]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest postfix-test
+  (testing "Basic programs"
+    (is (= ((postfix 0 1 2 3)) 3) "Only the top stack value is returned.")
+    (testing "with pop"
+      (is (= ((postfix 0 1 2 3 'pop)) 2))
+      (testing "and with swap"
+        (is (= ((postfix 0 1 2 'swap 3 'pop)) 1)))))
+  (testing "Basic argument handling"
+    (is (= ((postfix 2) 3 4) 3))
+    (testing "with pop and swap"
+      (is (= ((postfix 2 'swap) 3 4) 4))
+      (is (= ((postfix 3 'pop 'swap) 3 4 5))))))
