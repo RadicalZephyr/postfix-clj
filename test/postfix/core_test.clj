10c536745c1202dbf74735edf5dcaec8aa7d1761
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-14 16:34:12 -0400

    Fix the tests to use macro style syntax

--- test/postfix/core_test.clj
+++ test/postfix/core_test.clj
@@ -6,11 +6,11 @@
   (testing "Basic programs"
     (is (= ((postfix 0 1 2 3)) 3) "Only the top stack value is returned.")
     (testing "with pop"
-      (is (= ((postfix 0 1 2 3 'pop)) 2))
+      (is (= ((postfix 0 1 2 3 pop)) 2))
       (testing "and with swap"
-        (is (= ((postfix 0 1 2 'swap 3 'pop)) 1)))))
+        (is (= ((postfix 0 1 2 swap 3 pop)) 1)))))
   (testing "Basic argument handling"
     (is (= ((postfix 2) 3 4) 3))
     (testing "with pop and swap"
-      (is (= ((postfix 2 'swap) 3 4) 4))
-      (is (= ((postfix 3 'pop 'swap) 3 4 5))))))
+      (is (= ((postfix 2 swap) 3 4) 4))
+      (is (= ((postfix 3 pop swap) 3 4 5))))))
