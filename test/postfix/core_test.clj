e55738abfb68b952e41ca4c9bc7fb10d0c9ba885
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-14 19:13:04 -0400

    Attempt to define a macro for generating binary operator tests

--- test/postfix/core_test.clj
+++ test/postfix/core_test.clj
@@ -20,21 +20,24 @@
     (is (thrown-with-msg? clojure.lang.ExceptionInfo #"pop: empty stack"
                           ((postfix 0 1 pop pop))))))
 
-(deftest subcommand-test
-  (testing "Add command"
-    (is (= (add-cmd [3 4])
-           [7]))
-    (is (= (add-cmd [0 3 2])
-           [0 5]))
-    (is (thrown-with-msg? clojure.lang.ExceptionInfo
-                          #"add: not enough values on the stack"
-                          (add-cmd [1]))))
+(defmacro defbinary-op-test [cmd-name op]
+  (let [cmd-name (name cmd-name)
+        fn-name  (symbol (str cmd-name "-cmd"))
+        test-args [3 1]
+        op-result (vector (apply op (reverse test-args)))]
+    (prn "test-args:" test-args "\nop result:" op-result)
+    `(testing ~(str cmd-name " command")
+       (is (~'= (~fn-name ~test-args)
+              ~op-result))
+       (is (~'= (~fn-name ~(into [0] test-args))
+              ~(into [0] op-result)))
+       (is (~'thrown-with-msg? clojure.lang.ExceptionInfo
+                               (re-pattern ~(str cmd-name
+                                                 ": not enough values on the stack"))
+                               (~fn-name [1]))))))
 
-  (testing "Sub command"
-    (is (= (sub-cmd [1 2])
-           [-1]))
-    (is (= (sub-cmd [0 3 2])
-           [0 1]))
-    (is (thrown-with-msg? clojure.lang.ExceptionInfo
-                          #"sub: not enough values on the stack"
-                          (sub-cmd [1])))))
+(deftest subcommand-test
+  (defbinary-op-test add +)
+  (defbinary-op-test sub -)
+  (defbinary-op-test mul *)
+  (defbinary-op-test div /))
