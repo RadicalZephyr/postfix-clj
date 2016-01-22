77b210a6640f52393d675f7224d7c38ca8085a45
Geoff Shannon <geoffpshannon@gmail.com>
2016-01-21 19:58:44 -0600

    Fix up a bunch of random little things.

--- README.md
+++ README.md
@@ -5,13 +5,9 @@
 An implementation of the Postfix language from the first chapter of
 Design Concepts in Programming Languages.
 
-## Usage
-
-FIXME
-
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2014-2016 Geoff Shannon
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject postfix "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Implementation of the postfix language in clojure"
+  :url "https://github.com/RadicalZephyr/postfix-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
