7a9f6849663dc8ce1816dae4ae855b1bc2e47441
Geoff Shannon <geoffpshannon@gmail.com>
2016-01-26 10:50:12 -0600

    Inline generate-program-fn

--- src/postfix/compiler.clj
+++ src/postfix/compiler.clj
@@ -43,21 +43,17 @@
 (defn compile-ast [ast]
   ast)
 
-(defn generate-program-fn
-  ([compiled-program]
-   (generate-program-fn compiled-program (prog/args-used compiled-program)))
-
-  ([compiled-program num-args]
-   (let [program-args (prog/program-args* compiled-program num-args)
-         body (prog/program-body compiled-program)]
-     `(fn ~program-args ~body))))
-
 (defn make-executable-sequence [instructions]
   (let [ast (build-ast (prog/empty-program) instructions)
-        compiled-program (compile-ast ast)]
-    (generate-program-fn compiled-program)))
+        args-used (prog/args-used ast)
+        compiled-program (compile-ast ast)
+        program-args (prog/program-args* ast args-used)
+        body (prog/program-body ast)]
+    `(fn ~program-args ~body)))
 
 (defmacro postfix [num-args & instructions]
   (let [ast (build-ast (prog/empty-program num-args) instructions)
-        compiled-program (compile-ast ast)]
-    (generate-program-fn compiled-program num-args)))
+        compiled-program (compile-ast ast)
+        program-args (prog/program-args* ast num-args)
+        body (prog/program-body ast)]
+    `(fn ~program-args ~body)))
